23ef64e63381ab5eaf5db780551a2202eb4d1194
ifrahrao <ifrahnadeem17@yahoo.com>
2021-06-11 14:35:56 +0500

    Adding Authentications and Accounts

--- btre/settings.py
+++ btre/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'pages.apps.PagesConfig',
     'listings.apps.ListingsConfig',
     'realtors.apps.RealtorsConfig',
+    'accounts.apps.AccountsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -134,6 +135,11 @@ STATICFILES_DIRS = [
 #Media Folder Settings
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR:'danger'
+}
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
